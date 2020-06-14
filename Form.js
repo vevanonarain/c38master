84603d512f04e7a248b0a200e4dc7bfd1bbdaaee
vevanonarain <60396096+vevanonarain@users.noreply.github.com>
2020-06-14 21:15:20 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,39 @@
+class Form {
+    constructor() {
+      this.input=createInput("Name");
+      this.button=createButton('Submit and Play');
+      this.greeting=createElement('h2');
+    }
+  
+    hide(){
+      this.greeting.hide();
+      this.button.hide();
+      this.input.hide();
+    }
+  
+    display(){
+      var title = createElement('h2')
+      title.html("HURDLE RACE");
+      title.position(130, 0);
+      
+      this.input.position(displayWidth/2-10,displayHeight/2-80);
+      this.button.position(displayWidth/2+60,displayHeight/2);
+  
+      this.button.mousePressed(()=>{
+        this.input.hide();
+        this.button.hide();
+
+        runner.name = this.input.value();
+        runnerCount+=1;
+        runner.index=runnerCount;
+        
+        runner.update();
+        runner.updateCount(runnerCount);
+
+        this.greeting.html("Hello " + runner.name + " Welcome to the hurdle race. Please wait for the other players to join" );
+        this.greeting.position(40,40);
+      });
+  
+    }
+  }
+  
